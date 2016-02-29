90fa0c8ce7c451d87f6647feacf2fa7455057b30
Jason Bratt <jasonhero858@gmail.com>
2016-02-28 23:13:55 -0600

    comment

--- src/client/init.js
+++ src/client/init.js
@@ -7,6 +7,8 @@ import App from './components/App';
 //Grab the initial state set by the server.
 const initialState = window.INITIAL_STATE
 
+
+//Comment out the lines below and then run the app again, will anything get rendered?
 render((
   <App peopleList={initialState.peopleList}/>
 ),
